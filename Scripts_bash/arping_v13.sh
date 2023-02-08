685c4f1c745429be9cec4255b308db760c277039
Denis Korolkov <dvkorolkov@mail.ru>
2023-02-09 11:08:15 +1200

    removing false loops and checks in a function SUBNET and HOST

--- Scripts_bash/arping_v13.sh
+++ Scripts_bash/arping_v13.sh
@@ -33,24 +33,12 @@ if [[ -z "$SUBNET" ]]; then
     echo "\$SUBNET must be passed as 3-th positional argument"
       for SUBNET in {0..255}
       do
-         if [[ -z "$HOST" ]]; then
-           echo "\$HOST must be passed as 4-th positional argument"
-           for HOST in {1..255}
-           do
-               echo "[*] IP : ${PREFIX}.${SUBNET}.${HOST}"
-               arping -c 3 -I "$INTERFACE" "${PREFIX}.${SUBNET}.${HOST}" 2> /dev/null
-           done
-             [[ "$HOST" =~ ^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$ ]] \
-             || { echo  "ERROR: HOST must match DIGIT {1..255}"; exit 1; }
-           exit 1
-          fi
+            echo "[*] IP : ${PREFIX}.${SUBNET}.${HOST}"
+            arping -c 3 -I "$INTERFACE" "${PREFIX}.${SUBNET}.${HOST}" 2> /dev/null
       done
-               [[ "$SUBNET" =~ ^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$ ]] \
-               || { echo "ERROR: SUBNET must match DIGIT {0..255}"; exit 1; }
-     exit 1
 fi
 
-[[ "$SUBNET" =~ ^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$ ]] \
+[[ "$SUBNET" =~ ^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?)$ ]] \
 || { echo "ERROR: SUBNET must match DIGIT {0..255}"; exit 1; }
 
 if [[ -z "$HOST" ]]; then
@@ -60,10 +48,8 @@ if [[ -z "$HOST" ]]; then
           echo "[*] IP : ${PREFIX}.${SUBNET}.${HOST}"
           arping -c 3 -I "$INTERFACE" "${PREFIX}.${SUBNET}.${HOST}" 2> /dev/null
       done
-          [[ "$HOST" =~ ^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$ ]] \
-          || { echo  "ERROR: HOST must match DIGIT {1..255}"; exit 1; }
-    exit 1
 fi
+
 [[ "$HOST" =~ ^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$ ]] \
 || { echo  "ERROR: HOST must match DIGIT {1..255}"; exit 1; }
 arping -c 3 -I "$INTERFACE" "${PREFIX}.${SUBNET}.${HOST}" 2> /dev/null
